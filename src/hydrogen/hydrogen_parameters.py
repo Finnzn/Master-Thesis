125993fb1efed736df0020cff872108eb15ac5c9
Finnzn <finn@zurnieden.ch>
2026-09-22 09:45:20 +0200

    "Different h2 price"

--- src/hydrogen/hydrogen_parameters.py
+++ src/hydrogen/hydrogen_parameters.py
@@ -20,7 +20,7 @@ LIFETIME_HYDROGEN_YEARS = FixedParameter(
 )
 
 RETAIL_PRICE_HYDROGEN_EUR_PER_T = FixedParameter(
-    value=7_500.0,
+    value=2_000.0,
     unit="EUR/t",
     description="Retail price of hydrogen used in the hydrogen-sector setup.",
 )
